c2b51e201a1c91591ded2d30571b52b2666e3b31
tetianastyt <stytsenko.tania@gmail.com>
2020-05-18 22:34:09 +0300

    Updates

---
+++ precache-manifest.19814c32e75fb06bd2bb66aede757b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9f965ab7d57fb4d39791f4bb17db45",
+    "url": "/cryptoCurrency/index.html"
+  },
+  {
+    "revision": "9f97a4c6f4bf868fdc8a",
+    "url": "/cryptoCurrency/static/css/main.ddb32b23.chunk.css"
+  },
+  {
+    "revision": "7bcb6babc1a9eb6f1245",
+    "url": "/cryptoCurrency/static/js/2.e6468209.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cryptoCurrency/static/js/2.e6468209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f97a4c6f4bf868fdc8a",
+    "url": "/cryptoCurrency/static/js/main.1a9e91f1.chunk.js"
+  },
+  {
+    "revision": "03d1e8c6ea7f7043fc3a",
+    "url": "/cryptoCurrency/static/js/runtime-main.4adebcf1.js"
+  },
+  {
+    "revision": "cbb9274db5f913f578a4b0b579d74629",
+    "url": "/cryptoCurrency/static/media/Loading.cbb9274d.gif"
+  }
+]);
